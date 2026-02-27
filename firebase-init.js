ca67557aebd58b106a89e4ab0b924dba6a24e596
tawrez <133521265+tawrez@users.noreply.github.com>
2026-02-27 18:51:27 +0530

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,19 +1,18 @@
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBnYrIk289re79mTZbZMp-U_-AAVsuoOD8",
-    authDomain: "flair-webapp.firebaseapp.com",
-    projectId: "flair-webapp",
-    storageBucket: "flair-webapp.firebasestorage.app",
-    messagingSenderId: "542875968797",
-    appId: "1:542875968797:web:fdea947b757444ba1c6df4",
-    measurementId: "G-RC9XEF2PM3"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBnYrIk289re79mTZbZMp-U_-AAVsuoOD8",
+  authDomain: "flair-webapp.firebaseapp.com",
+  projectId: "flair-webapp",
+  storageBucket: "flair-webapp.firebasestorage.app",
+  messagingSenderId: "542875968797",
+  appId: "1:542875968797:web:fdea947b757444ba1c6df4",
+  measurementId: "G-RC9XEF2PM3"
+};
 
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-  window.flairFirebase = { app, auth };
-</script>
+window.flairFirebase = { app, auth, RecaptchaVerifier, signInWithPhoneNumber };
+
